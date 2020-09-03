e4f02eaa6075b3194837354d5e75c453e130758a
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-09-03 18:48:44 +0300

    Updates

---
+++ precache-manifest.bee7c1c238243d2f1394ec0af4258bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fe9f5e9ad2927cdadf4b46fa687e61",
+    "url": "/goit-react-hw-01/index.html"
+  },
+  {
+    "revision": "982b48c5d4ce02b1b6cd",
+    "url": "/goit-react-hw-01/static/css/main.0014ac64.chunk.css"
+  },
+  {
+    "revision": "665740dc1e308c227505",
+    "url": "/goit-react-hw-01/static/js/2.6dc0cd31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01/static/js/2.6dc0cd31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982b48c5d4ce02b1b6cd",
+    "url": "/goit-react-hw-01/static/js/main.9e39b6da.chunk.js"
+  },
+  {
+    "revision": "e389f2825024a6328db0",
+    "url": "/goit-react-hw-01/static/js/runtime-main.064837fa.js"
+  }
+]);
